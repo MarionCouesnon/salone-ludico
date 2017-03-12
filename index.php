defbea856023567bcb62f46cdb391f690cb4a580
marioncouesnon <marion@craftsmen.io>
2017-03-12 13:05:55 +0100

    raw skeleton

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php include 'header.php';?>
+<div id="hero">
+  <p class="headline">Designed distractions that coerce us into playful and shared forms of interaction</p>
+</div>
+<div class="text">
+  <p class="hastags">#saloneludico<br>#SalonedelMobile<br>#Milano</p>
+  <p>Our “Salone” invites visitors to discover twelve projects conceived as ludic antidotes to a world filled with screens. Designed distractions that coerce us into playful and shared forms of interaction. Everyone plays. Electronic. Analog. Social. Play is a form of connection. Social platforms feign connecting us, but in the end they just leave us in limbo. Inversely, play is superficial by design, while secretly inviting us into a deeper understanding of each other and of our own limits.</p>
+
+  <p>Designed and developed by the Media Design Master of the –HEAD Genève within an identity designed by students of Visual Communication, the Salone Ludico invites visitors to explore the nature of play. Projects range in scope and scale from robotic mixologists and domotic hi-fi to infinite rooms where space itself becomes the play station. For the more adventurous, an underground club proposes a redefinition of the seedy games room with updated parlour games of strategy and chance.</p>
+</div>
+<div class="teaser-img-section">
+  <div class="penultimo-project">
+    <p class="project-name">Penultimo<p>
+    <img src="img/penultimo.jpg" alt="penultimo teaser image">
+  </div>
+  <div class="ximoan-project">
+    <p class="project-name">Ximoan<p>
+    <img src="img/ximoan.jpg" alt="ximoan teaser image">
+  </div>
+  <div class="gravity-project">
+    <p class="project-name">Gravity<p>
+    <img src="img/gravity.jpg" alt="gravity teaser image">
+  </div>
+  <div class="penumbra-project">
+    <p class="project-name">Penumbra<p>
+    <img src="img/penumbra.jpg" alt="penumbra teaser image">
+  </div>
+</div>
+<?php include 'footer.php';?>
