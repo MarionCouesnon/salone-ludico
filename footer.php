3342bae0bf7411106f6c49473877313c37791fdd
marioncouesnon <marion@craftsmen.io>
2017-03-12 20:13:45 +0100

    first website version

--- footer.php
+++ footer.php
@@ -1,5 +1,16 @@
 	<footer>
-		
+		<div class="head-credential">
+			<img src="img/logo-head.png" alt="logo HEAD" class="logo-head">
+		</div>
+		<p class="credits">© 2017 —HEAD Genève, Haute école d'art et de design</p>
 	</footer>
+	<!-- Scripts -->
+<script src="js/deploy/parallax.js"></script>
+<script>
+	// Pretty simple huh?
+	var scene = document.getElementById('scene');
+	var parallax = new Parallax(scene);
+	parallax.scalar(0, 4);
+</script>
 </body>
 </html>
--- header.php
+++ header.php
@@ -1,10 +1,13 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<title></title>
+	<title>Salone Ludico</title>
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 	<meta name="mobile-web-app-capable" content="yes">
 	<meta name="apple-mobile-web-app-capable" content="yes">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="stylesheet" href="stylesheet/style.css">
 </head>
+<body>
