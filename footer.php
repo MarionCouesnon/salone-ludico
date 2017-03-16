4d8c46ffeeed655f6ec3d3af265ff82e68c9575c
marioncouesnon <marion@craftsmen.io>
2017-03-16 21:00:11 +0100

    version 2- improved layout

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
-	<footer>
+	<footer class="main">
 		<div class="head-credential">
-			<img src="img/logo-head.png" alt="logo HEAD" class="logo-head">
+			<a href="https://www.hesge.ch/head/" target="_blank" class="logo-head"><img src="img/logo-head.png" alt="logo HEAD" class="img-head"></a>
 			<p class="credits">© 2017 —HEAD Genève, Haute école d'art et de design</p>
 		</div>
 
