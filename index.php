85e1a1d2b05fcc41120c0fc61695480e9ddc20e7
marioncouesnon <marion@craftsmen.io>
2017-03-23 17:20:11 +0100

    remove all img and add responsive video

--- index.php
+++ index.php
@@ -9,62 +9,55 @@
   </div>
 </div>
 
-<div class="main baseline">
+<div class="main">
+
   <h1 class="headline">Designed distractions to coerce us into playful and shared forms of interaction</h1>
-</div>
 
-<div class="teaser-img-section">
-  <div class="projects horizontal" id="penultimo">
+  <div class="video-wrapper">
+    <iframe src="https://player.vimeo.com/video/209437425?byline=0&portrait=0" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen class="video"></iframe>
   </div>
-</div>
 
-<div class="main">
-  <div class="text">
-    <h1 class="title">Salone internazionale del Mobile di Milano<br>Brera Design District, from 4 to 9 April 2017</h1>
-    <div class="text-wrapper">
-      <div class="to-twitter">
-        <a href="https://twitter.com/search?f=tweets&q=%23saloneludico&src=typd" class="hashtags">#saloneludico</a>
-        <a href="https://twitter.com/search?q=%23fuorisalone2017&src=typd" class="hashtags">#fuorisalone2017</a>
-        <a href="https://twitter.com/search?q=%23breradesigndistrict&src=typd" class="hashtags">#breradesigndistrict</a>
+  <div class="overview-presentation">
+    <div class="presentation">
+      <h1 class="title">Salone internazionale del Mobile di Milano<br>Brera Design District, from 4 to 9 April 2017</h1>
+      <div class="presentation-wrapper">
+        <div class="to-twitter">
+          <a href="https://twitter.com/search?f=tweets&q=%23saloneludico&src=typd" class="hashtags">#saloneludico</a>
+          <a href="https://twitter.com/search?q=%23fuorisalone2017&src=typd" class="hashtags">#fuorisalone2017</a>
+          <a href="https://twitter.com/search?q=%23breradesigndistrict&src=typd" class="hashtags">#breradesigndistrict</a>
+        </div>
+        <div class="subject-container">
+        <p class="subject">Our “Salone” invites visitors to discover twelve projects conceived as ludic antidotes to a world filled with screens. Designed distractions that coerce us into playful and shared forms of interaction. Everyone plays. Electronic. Analog. Social. Play is a form of connection. Social platforms feign connecting us, but in the end they just leave us in limbo. Inversely, play is superficial by design, while secretly inviting us into a deeper understanding of each other and of our own limits.</p>
+        <p class="subject">Designed and developed by the Media Design Master of the –HEAD Genève within an identity designed by students of Visual Communication, the Salone Ludico invites visitors to explore the nature of play. Projects range in scope and scale from robotic mixologists and domotic hi-fi to infinite rooms where space itself becomes the play station. For the more adventurous, an underground club proposes a redefinition of the seedy games room with updated parlour games of strategy and chance.</p>
+        </div>
       </div>
-      <div class="text-container">
-      <p class="subject">Our “Salone” invites visitors to discover twelve projects conceived as ludic antidotes to a world filled with screens. Designed distractions that coerce us into playful and shared forms of interaction. Everyone plays. Electronic. Analog. Social. Play is a form of connection. Social platforms feign connecting us, but in the end they just leave us in limbo. Inversely, play is superficial by design, while secretly inviting us into a deeper understanding of each other and of our own limits.</p>
-      <p class="subject">Designed and developed by the Media Design Master of the –HEAD Genève within an identity designed by students of Visual Communication, the Salone Ludico invites visitors to explore the nature of play. Projects range in scope and scale from robotic mixologists and domotic hi-fi to infinite rooms where space itself becomes the play station. For the more adventurous, an underground club proposes a redefinition of the seedy games room with updated parlour games of strategy and chance.</p>
+    </div>
+  </div>
+
+  <div class="exhibition-information">
+    <h2 class="title">Opening Tuesday 4 April, 18:00–21:00</h2>
+    <div class="info-wrapper">
+
+      <div class="info-container">
+        <h3>Exhibition</h3>
+        <p>Tuesday 4 to Sunday 9 April 2017, 11:00–20:00<br>
+        Press Preview  Monday 3 April, 14:00-16:00<p>
       </div>
-    </div>
-  </div>
-</div>
 
-<div class="teaser-img-section">
-  <div class="projects vertical" id="ximoan">
-  </div>
-  <div class="projects horizontal" id="penumbra">
-  </div>
-  <div class="projects vertical" id="gravity">
-  </div>
-</div>
+      <div class="info-container">
+        <h3>Location</h3>
+        <p>Brera Design District
+        c/o Mimmo Scognamiglio
+        Artecontemporanea<br>
+        Via Goito 7, 20121 Milano</p>
+      </div>
 
-<div class="main">
-  <h2 class="title">Opening Tuesday 4 April, 18:00–21:00</h2>
-  <div class="information">
-    <div class="info-container">
-      <h3>Exhibition</h3>
-      <p>Tuesday 4 to Sunday 9 April 2017, 11:00–20:00<br>
-      Press Preview  Monday 3 April, 14:00-16:00<p>
-    </div>
+      <div class="info-container">
+        <h3>Access</h3>
+        <p>M2 Moscova/Lanza M3 Turati<br>
+        Bus 43, 61, 94</p>
+      </div>
 
-    <div class="info-container">
-      <h3>Location</h3>
-      <p>Brera Design District
-      c/o Mimmo Scognamiglio
-      Artecontemporanea<br>
-      Via Goito 7, 20121 Milano</p>
-    </div>
-
-    <div class="info-container">
-      <h3>Access</h3>
-      <p>M2 Moscova/Lanza M3 Turati<br>
-      Bus 43, 61, 94</p>
     </div>
   </div>
 </div>
