d324c65350524df6e65d2f77685e2d5d1b70a360
Gordan Savicic <gordo@yugo.at>
2017-03-26 14:02:30 +0200

    added gyrotest for iOs

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,1 +1,1 @@
-"use strict";var scene=document.getElementById("scene"),parallax=new Parallax(scene);parallax.scalar(0,50);
+"use strict";var deviceOrientationData={alpha:0,beta:0,gamma:0};window.addEventListener("deviceorientation",function(a){deviceOrientationData.alpha=a.alpha,deviceOrientationData.beta=a.beta,deviceOrientationData.gamma=a.gamma,document.getElementById("alpha").innerHTML=deviceOrientationData.alpha,document.getElementById("beta").innerHTML=deviceOrientationData.beta,document.getElementById("gamma").innerHTML=deviceOrientationData.gamma});
