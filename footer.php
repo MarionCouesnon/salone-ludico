57b7160c9c98df3110f14c8258076fc170e1550b
marioncouesnon <marion@craftsmen.io>
2017-03-27 00:43:09 +0200

    change credit layout

--- footer.php
+++ footer.php
@@ -1,6 +1,60 @@
 	<footer class="main">
+		<h2 class="title">Credits</h2>
 		<div class="head-credential">
-			<a href="https://www.hesge.ch/head/" target="_blank" class="logo-head"><img src="img/logo-head.png" alt="logo HEAD" class="img-head"></a>
+			<div class="direction-list-wrapper">
+				<ol class="direction-credits">
+					<li class=""><h4>Director HEAD – Genève</h4>
+					Jean-Pierre Greff</li>
+					<li class=""><h4>Department Direction, Master Media Design</h4>
+					Nicolas Nova, Gordan Savičić</li>
+					<li class=""><h4>Project Direction</h4>
+					Douglas Edric Stanley</li>
+				</ol>
+				<ol class="direction-credits">
+					<li><h4>Project Assistant</h4>
+					Laura Couto Rosado</li>
+					<li><h4>Interaction Design Direction</h4>
+					Étienne Mineur</li>
+					<li><h4>Industrial Design Direction</h4>
+					Laurent Bolli</li>
+				</ol>
+				<ol class="direction-credits">
+					<li><h4>Visual Design Direction</h4>
+					Anette Lenz</li>
+					<li><h4>Technical Development Direction</h4>
+					Pierre Rossel</li>
+					<li><h4>Scenography Direction</h4>
+					Pascal Berger</li>
+				</ol>
+			</div>
+			<div class="additionnal-wrapper">
+				<h4>Additional Production</h4>
+				<p class="as-li">Alexandre Burdin, Frederic Butor-Blamont, Claudio Cicchini, Robert Favre, Pierre-Alain Giesser, Sébastien Pitteloud, Xavier Plantevin, Daniel Sciboz, Alexandre Simian, Sergio Streun, Nicolas Wagnières, Mathias Zieba</p>
+			</div>
+		</div>
+		<div class="social-networks">
+			<div class="social-wrapper">
+				<div class="social-logo-container">
+					<a href="https://twitter.com/headmediadesign" class="social" target="_blank">
+						<img src="img/twitter-logo.png" alt="logo twitter">
+					</a>
+					<a href="https://vimeo.com/headmediadesign" class="social" target="_blank">
+						<img src="img/vimeo-logo.png" alt="logo vimeo">
+					</a>
+					<a href="https://www.instagram.com/headmediadesign/" class="social" target="_blank">
+						<img src="img/insta-logo.png" alt="logo instagram">
+					</a>
+				</div>
+				<div class="hashtag-section">
+					<a href="https://twitter.com/search?f=tweets&q=%23headmediadesign&src=typd" >#headmediadesign</a>
+				</div>
+			</div>
+			<div class="logo-head">
+				<a href="https://www.hesge.ch/head/" target="_blank" class="logo-head-container">
+					<img src="img/logo-head.png" alt="logo HEAD" class="img-head">
+				</a>
+			</div>
+		</div>
 			<p class="credits">© 2017 HEAD–Genève, Haute école d'art et de design</p>
 		</div>
 
